0dff56b996d66099c62e171b2b7c241d27ec95e2
Morbanaa <theodorerodd@gmail.com>
2026-05-17 15:02:12 -0500

    Set up manager class with empty world gen and render game methods. Also created the move cursor method for better UI. Created the player class and created a player object in the constructor

--- classes.py
+++ classes.py
@@ -1,1 +1,25 @@
-ECHO is on.
+import sys
+
+class Game_Manager():
+    def __init__(self,game_width,game_height):
+        self.game_width = game_width
+        self.game_height = game_height
+        self.player = Player(game_width//2,game_height//2)
+
+    # Creates world onces at start of game
+    def world_gen(self):
+        pass
+
+    # Renders world each frame of game
+    def render_game(self):
+        pass
+
+    # Sets screen for next frame
+    def clear_move_cursor():
+        sys.stdout.write("\033[H")
+        sys.stdout.flush()
+
+class Player():
+    def __init__(self,xpos,ypos):
+        self.xpos = xpos
+        self.ypos = ypos
