cb050a438ab37bb6150866ac1624e1d81076991d
Morbanaa <theodorerodd@gmail.com>
2026-05-17 18:08:53 -0500

    Removed player class and moved to player.py. classes.py now imports Player class

--- classes.py
+++ classes.py
@@ -4,6 +4,7 @@
 
 import sys
 import keyboard
+from player import Player
 
 # Colors
 GREEN        = '\033[32m'
@@ -46,24 +47,4 @@ class Game_Manager():
         sys.stdout.write("\033[H")
         sys.stdout.flush()
     
-    
-
-class Player():
-    def __init__(self,xpos,ypos):
-        self.xpos = xpos
-        self.ypos = ypos
-            
-    # Player Movement
-    def update_player(self,game_map):
-        # Move Up
-        if keyboard.is_pressed("W") and game_map[self.ypos -1][self.xpos] != "@":
-            self.ypos -= 1
-        # Move Down
-        if keyboard.is_pressed("S") and game_map[self.ypos + 1][self.xpos] != "@":
-            self.ypos += 1
-        # Move Left
-        if keyboard.is_pressed("A") and game_map[self.ypos][self.xpos -1] != "@":
-            self.xpos -= 1
-        # Move Right
-        if keyboard.is_pressed("D") and game_map[self.ypos][self.xpos +1] != "@":
-            self.xpos += 1
+   
