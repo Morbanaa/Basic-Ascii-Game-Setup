dd3d83786139d25ae0572cb0c4dbe86eb941788b
Morbanaa <theodorerodd@gmail.com>
2026-05-17 15:08:32 -0500

    Set up main function creating game manager object and calling world gen. In the main game loop it calls the render method and the move cursor method

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-ECHO is on.
+# Teddy Rodd
+# Morbanaa Studios
+# Basic Ascii Game Setup
+
+from classes import Game_Manager
+
+def main():
+    game_manager = Game_Manager(80,25) # Pass in Width Height
+    game_manager.world_gen()
+    while True:
+        game_manager.render_game()
+        game_manager.clear_move_cursor()
+
+
+if __name__ == "__main__":
+    main()
