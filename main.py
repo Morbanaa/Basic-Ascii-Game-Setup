7048fcfab7ed442ea50f3ecd122e821717a1d382
Morbanaa <theodorerodd@gmail.com>
2026-05-17 17:03:36 -0500

    Set up game clock so everything runs at a consistant rate

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 # Morbanaa Studios
 # Basic Ascii Game Setup
 
+import time
 from classes import Game_Manager
 
 def main():
+    game_speed = .05 # Smaller number = faster game speed
     game_manager = Game_Manager(80,25) # Pass in Width Height
     game_manager.world_gen()
     while True:
+        # Update
         game_manager.update_player()
+
+        # Render
         game_manager.render_game()
-        game_manager.clear_move_cursor()
+
+        # Util
+        time.sleep(game_speed) # Game Clock
+        game_manager.clear_move_cursor() # Moves cursor to top of console
 
 
 if __name__ == "__main__":
