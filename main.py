d2d499896b09286f54ecce007f850f622ef783fb
Morbanaa <theodorerodd@gmail.com>
2026-05-17 18:31:45 -0500

    Added the abilty to quit the game by pressing R. Also added clear screen function that wipes the console before saying goodbye

--- main.py
+++ main.py
@@ -3,13 +3,21 @@
 # Basic Ascii Game Setup
 
 import time
-from classes import Game_Manager
+import keyboard
+import os
+import platform
+from game_manager import Game_Manager
 
 def main():
     game_speed = .05 # Smaller number = faster game speed
     game_manager = Game_Manager(80,25) # Pass in Width Height
     game_manager.world_gen()
     while True:
+        # Update
+        if keyboard.is_pressed("Q"):
+            clear_screen()
+            break
+
         # Render
         game_manager.render_game()
 
@@ -17,6 +25,14 @@ def main():
         time.sleep(game_speed) # Game Clock
         game_manager.clear_move_cursor() # Moves cursor to top of console
 
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 if __name__ == "__main__":
     main()
+
+    # Last message
+    print("Thanks for playing!\n")
