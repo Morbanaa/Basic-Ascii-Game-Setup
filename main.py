fcd47c8832c068d2580912b9abb4e95bfbb17f0f
Morbanaa <theodorerodd@gmail.com>
2026-05-17 18:04:35 -0500

    Update will now be handled within render in classes.py

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ def main():
     game_manager = Game_Manager(80,25) # Pass in Width Height
     game_manager.world_gen()
     while True:
-        # Update
-        game_manager.update_player()
-
         # Render
         game_manager.render_game()
 
